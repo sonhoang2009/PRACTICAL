0426190dc9af17815e3603978a72a5fab4f2ba3a
sonhoang2009 <sonset1996@gmail.com>
2021-06-14 20:20:44 +0700

    practical
    pratical

---
+++ PRACTICAL/Models/EmployeeList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRACTICAL.Models
+{
+    public class EmployeeList
+    {
+        public string name { get; set; }
+
+        public string role { get; set; }
+
+        public string birthyear { get; set; }
+    }
+
+    public class Root
+    {
+        public List<EmployeeList> employeeLists { get; set; }
+    }
+}
